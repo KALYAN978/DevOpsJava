2e4f9606d7aed984457f905bc0d3e9433e0f2d30
Kalyan <pkalyanjs@gmail.com>
2023-11-28 16:00:36 +0530

    Java Practised handon on FlowControl has been added

---
+++ FlowControl/src/IfElseDemo.java
@@ -0,0 +1,21 @@
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+
+		//Finding which number is greater
+		
+		int x = 10,y = 20;
+		
+		if(x>y) {
+			System.out.println("X is greater");
+		}
+		else if(y>x){
+			System.out.println("Y is greater");
+		}
+		else {
+			System.out.println("Both X and Y are Equal");
+		}
+	}
+
+}
---
+++ FlowControl/src/SwitchDemo.java
@@ -0,0 +1,30 @@
+
+public class SwitchDemo {
+
+	public static void main(String[] args) {
+
+		int x = 4;
+		
+		switch (x) {
+		
+		case 1:
+			System.out.println("Case 1");
+			break;
+		
+		case 2:
+			System.out.println("case 2");
+			break;
+		case 3:
+			System.out.println("case 3");
+			break;
+		case 4:
+			System.out.println("case 4");
+			break;
+		default:
+			System.out.println("default");
+			
+			
+		}	
+	}
+
+}
