e6f50adb74c3e3808aa66112dacf7cdb89f2713c
Kalyan <pkalyanjs@gmail.com>
2023-11-30 15:15:15 +0530

    Area of a circle in java

--- PractisePrograms/src/practiseprograms/CalculateCircleArea.java
+++ PractisePrograms/src/practiseprograms/CalculateCircleArea.java
@@ -1,10 +1,35 @@
 package practiseprograms;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class CalculateCircleArea {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		
+		int radius = 0;
+		
+		System.out.println("Enter a radius: ");
+		
+		try {
+			//get the radius from the console
+			
+			Scanner sc = new Scanner(System.in);
+			
+			radius = sc.nextInt();
+			
+		}
+		//if Invalid value has entered
+		catch(NumberFormatException ne) {
+			System.out.println("Invalid Radius value: "+ ne);
+		}
 
+		double area = Math.PI * radius * radius;
+		
+		System.out.println(area);
+		
 	}
 
 }
