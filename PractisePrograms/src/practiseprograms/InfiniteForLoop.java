a3a8ff13a4cc7ba8bdd2bbd16122d79529082a88
Kalyan <pkalyanjs@gmail.com>
2023-12-01 15:44:18 +0530

    Practised java switch and Nested statements has added

---
+++ PractisePrograms/src/practiseprograms/InfiniteForLoop.java
@@ -0,0 +1,12 @@
+package practiseprograms;
+
+public class InfiniteForLoop {
+
+	public static void main(String[] args) {
+
+		for(;;) {
+			System.out.println("Hello");
+		}
+	}
+
+}
---
+++ PractisePrograms/src/practiseprograms/SwitchStatement.java
@@ -0,0 +1,31 @@
+package practiseprograms;
+
+public class SwitchStatement {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0;i < 3; i++) {
+			switch(i) {
+			case 0:
+				System.out.println("0");
+				break;
+			case 1:
+				System.out.println("1");
+				break;
+			case 2:
+				System.out.println("2");
+				break;
+			case 3:
+				System.out.println("3");
+				break;
+			case 4:
+				System.out.println("4");
+				break;
+			default:
+				System.out.println("01234");
+			}
+		}
+
+	}
+
+}
