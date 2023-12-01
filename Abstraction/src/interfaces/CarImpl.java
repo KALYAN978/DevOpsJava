9d2a3ff4905a5faa11973969d983943b66324559
Kalyan <pkalyanjs@gmail.com>
2023-12-01 14:48:45 +0530

    added the practised handosn on java

---
+++ Abstraction/src/interfaces/CarImpl.java
@@ -0,0 +1,19 @@
+package interfaces;
+
+public class CarImpl implements Car{
+
+	@Override
+	public void go() {
+		System.out.println("Inside Car Go: ");
+		
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("Inside Car Stop: ");
+		
+	}
+	
+	
+
+}
---
+++ Abstraction/src/interfaces/Test.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		CarImpl carImpl = new CarImpl();
+		
+		carImpl.go();
+		carImpl.stop();
+		}
+
+}
