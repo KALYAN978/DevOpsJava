7388ce5b863141585c802f46a23eabdf2c1817f5
Kalyan <pkalyanjs@gmail.com>
2023-12-01 23:20:16 +0530

    java practise

---
+++ PractisePrograms/src/javacoreandadvancewithanimation/JavaPackageExample.java
@@ -0,0 +1,14 @@
+//package practiseprograms;
+
+
+package javacoreandadvancewithanimation;
+
+public class JavaPackageExample {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java: learn with Animation videos[core and advance]");
+
+		
+	}
+
+}
