4e4d4d804145f127ed3f6bb0c90465751602ec1b
Kalyan <pkalyanjs@gmail.com>
2023-12-13 15:26:55 +0530

    code if-else code has been added

---
+++ Programs/src/If_else.java
@@ -0,0 +1,18 @@
+
+public class If_else {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Tim");
+		
+		
+		boolean isAlien = false;
+		if (isAlien == true)
+		{
+			System.out.println("Its not an alien");
+		}
+		else {
+			System.out.println("Its an alien");
+		}
+	}
+
+}
