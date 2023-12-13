a88f36d87dcded0537046818d731b521767f001c
Kalyan <pkalyanjs@gmail.com>
2023-12-13 15:50:32 +0530

    Ternary Operator practise code has been added

---
+++ Programs/src/TernaryOperator.java
@@ -0,0 +1,21 @@
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		
+//		String makeOfCar = "Volkswagen";
+//		boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+//		
+//		if (isDomestic) {
+//			System.out.println("This car is Domestic to our country");
+//		}
+		
+		
+		int ageOfClient = 20;
+		String ageText = (ageOfClient >= 18) ? "Over Eighteen" : "Still a kid";
+		System.out.println("Our client is "+ ageText);
+		
+
+	}
+
+}
