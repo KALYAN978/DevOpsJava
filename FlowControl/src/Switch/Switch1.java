e8101ff7d8d397fa9da96403db5210b9bf17c962
Kalyan <pkalyanjs@gmail.com>
2023-12-15 11:44:57 +0530

    added flow control code

---
+++ FlowControl/src/Switch/Switch1.java
@@ -0,0 +1,25 @@
+package Switch;
+
+public class Switch1 {
+
+	public static void main(String[] args) {
+		
+		char charValue = 'A';
+		switch(charValue) {
+		case 'A':
+			System.out.println("A is able");
+			break;
+		case 'B':
+			System.out.println("B is tom");
+			break;
+		case 'C':
+			System.out.println("C is Cuhs");
+			break;
+		default:
+			System.out.println("Default");
+			break;
+		}
+
+	}
+
+}
