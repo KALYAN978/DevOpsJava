184d822370629e10efe96a0aa6707a371fdfd170
Kalyan <pkalyanjs@gmail.com>
2023-12-19 12:10:01 +0530

    switch case practise code has been added

---
+++ CodingExercise/src/methods/Switch.java
@@ -0,0 +1,51 @@
+package methods;
+
+import methodoverloading.Main;
+
+public class Switch {
+	
+	public static void printNumberInWord(int number) {
+		switch(number) {
+		case 0:
+			System.out.println("ZERO");
+			break;
+		case 1:
+			System.out.println("One");
+			break;
+		case 2:
+			System.out.println("Two");
+			break;
+		case 3:
+			System.out.println("Three");
+			break;
+		case 4:
+			System.out.println("Four");
+			break;
+		case 5:
+			System.out.println("Three");
+			break;
+		case 6:
+			System.out.println("Six");
+			break;
+		case 7:
+			System.out.println("Seven");
+			break;
+		case 8:
+			System.out.println("Eight");
+			break;
+		case 9:
+			System.out.println("Nine");
+			break;
+		default:
+			System.out.println("Other");
+			break;
+		}
+		
+		printNumberInWord(4);
+
+		
+		
+		
+	}
+
+}
