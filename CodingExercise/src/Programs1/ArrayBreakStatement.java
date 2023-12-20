dd1a6cb7656d2fa783a3492d0935d2d2dabd9ffe
Kalyan <pkalyanjs@gmail.com>
2023-12-20 14:55:05 +0530

    code has addeed

---
+++ CodingExercise/src/Programs1/ArrayBreakStatement.java
@@ -0,0 +1,15 @@
+package Programs1;
+
+public class ArrayBreakStatement {
+
+	public static void main(String[] args) {
+		
+		int Array[] = new int[] {1,2,3,4,5,6,7,8,9,10};
+		System.out.println("Elements less than 9 are :  ");
+		for(int i=0;i<Array.length;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
