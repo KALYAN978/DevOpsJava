4657bdf262b0044c61e5f93578fb3e42502071f7
Kalyan <pkalyanjs@gmail.com>
2023-12-20 13:27:11 +0530

    Loops code has been added

---
+++ CodingExercise/src/Programs1/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package Programs1;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		
+		
+		int i = 10;
+		do {
+			System.out.println("Printing One By One : ");
+			System.out.println(i);
+		}while(i<1);
+	}
+
+}
---
+++ CodingExercise/src/Programs1/InfiniteWhileloop.java
@@ -0,0 +1,13 @@
+package Programs1;
+
+public class InfiniteWhileloop {
+
+	public static void main(String[] args) {
+		int i = 10;
+		while(i>1) {
+			System.out.println(i);
+		}
+	}
+
+
+}
