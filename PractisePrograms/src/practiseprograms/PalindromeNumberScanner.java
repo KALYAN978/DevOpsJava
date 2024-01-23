1b40e49ecbbf00f7796f0abf322a87960d252505
Kalyan <pkalyanjs@gmail.com>
2024-01-23 13:03:38 +0530

    practised code has been added

---
+++ PractisePrograms/src/practiseprograms/PalindromeNumberScanner.java
@@ -0,0 +1,41 @@
+package practiseprograms;
+
+import java.util.Scanner;
+
+// write a palindrome program by taking input from the user 
+
+public class PalindromeNumberScanner {
+	
+	
+	public static void main(String[] args) {
+		
+		String original, reverse = "";
+		
+		System.out.println("Enter a String/numnber to check whether it is a palindrome or not!!!");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		original = sc.next();
+		
+		int length = original.length();
+		
+		for(int i = length - 1; i >= 0; i--) {
+			reverse = reverse + original.charAt(i);
+		}
+		if(original.equals(reverse))
+		{
+			System.out.println("Entered String is palindrome");
+			
+		}
+		else {
+			System.out.println("Entered string is not output");
+		}
+		
+		
+		
+		
+
+	}
+
+}
+  
