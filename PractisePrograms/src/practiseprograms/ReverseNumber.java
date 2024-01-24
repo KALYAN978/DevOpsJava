f57159d81d4da15d3555ad093c80f73a8325f2ff
Kalyan <pkalyanjs@gmail.com>
2024-01-24 12:31:48 +0530

    Code has been modified

--- PractisePrograms/src/practiseprograms/ReverseNumber.java
+++ PractisePrograms/src/practiseprograms/ReverseNumber.java
@@ -4,44 +4,45 @@ public class ReverseNumber {
 
 	public static void main(String[] args) {
 		
-//		// Original Number
+//		
 //		int number = 1234;
 //		
 //		int reversedNumber = 0;
-//		int temp=0;
+//		int temp = 0;
+//
 //		
-//		while(number > 0) {
-//			// use the modulus operator to strip the last digit
-//			temp = number%10; //Created the reversed Number
-//			
+//		while (number > 0) {
+//			//use the modulus operator to strip off the last digit
+//			temp = number % 10;  //created the reverseNumber
 //			reversedNumber = reversedNumber * 10 + temp;
 //			number = number/10;
-//			
-//			//Output the reversed Number
-//			System.out.println("Reversed Number is: "+ reversedNumber);
-//			
 //		}
+//		
+//		//Output the result 
+//		System.out.println("Reversed Number is : " + reversedNumber);
 		
 		
 		
-		
-		
-		int number = 1234;
-		
+		int number = 123235;
 		int reversedNumber = 0;
-		int temp = 0;
-
 		
-		while (number > 0) {
-			//use the modulus operator to strip off the last digit
-			temp = number % 10;  //created the reverseNumber
-			reversedNumber = reversedNumber * 10 + temp;
-			number = number/10;
+		int temp = 0;
+		
+		while(number > 0) {
+			// Use the modulus operator to strip off the lastdigit
+			temp = number % 10;    //Created the reversed Number
+			
+			number = number /10;
+			
 		}
 		
-		//Output the result 
-		System.out.println("Reversed Number is : " + reversedNumber);
+		//output the result
+		System.out.println(reversedNumber);
 
 	}
+	
 
+		
+		
+		
 }
