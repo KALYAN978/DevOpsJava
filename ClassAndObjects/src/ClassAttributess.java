0826b95eefaf27d5d0053820c1f155e4dcce2f91
Kalyan <pkalyanjs@gmail.com>
2024-02-09 13:04:40 +0530

    code has been added

---
+++ ClassAndObjects/src/ClassAttributess.java
@@ -0,0 +1,23 @@
+
+
+class Dog{
+	String breed = "German Shepard";
+	int age = 2;
+	String color = "Black";
+}
+
+
+public class ClassAttributess {
+
+	public static void main(String[] args) {
+		
+		Dog obj = new Dog();
+		
+		System.err.println(obj.age);
+		System.out.println(obj.breed);
+		System.out.println(obj.color);
+		
+
+	}
+
+}
